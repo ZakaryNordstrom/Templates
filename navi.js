b4170bb22f466e2e0dff7468bedd7104e28aa13b
ZakaryNordstrom <31268306+ZakaryNordstrom@users.noreply.github.com>
2018-02-12 16:19:20 -0800

    Delete navi.js

--- navi.js
+++
@@ -1,6 +0,0 @@
-$(".hamburger").click(function () {
-  $(".menu").toggleClass("expandMenu");
-  $(".menu").toggleClass("button-height");
-  $("#siteHeader").toggleClass("mobile-height");
-  $(".hamburger").toggleClass("is-active");
-});
