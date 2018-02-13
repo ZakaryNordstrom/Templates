6fac643afd791f8b7bf75078112cfd2fb73d54fe
ZakaryNordstrom <31268306+ZakaryNordstrom@users.noreply.github.com>
2018-02-13 11:47:49 -0800

    Delete footer.php

--- components/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-  <div class="sm-icons">[social media] [social media]</div>
-  <div class="copyright"><p>©<?php echo date("Y");?> Zak Nordstrom</p></div>
-
-</footer>
-</body>
-</html>
